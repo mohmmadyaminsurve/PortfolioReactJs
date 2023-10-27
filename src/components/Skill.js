26211f59f371f5c320c2294fe09f7965ba7d9ede
mohmmadyaminsurve <yaminsurve909@gmail.com>
2023-10-28 00:21:04 +0530

    skill section on scroll visible done

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Skill.scss";
 import Skillimage from "./Images/Skillimage.png";
 import Group from "./Images/Group1.svg";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
-import { useRef } from "react";
+import { useInView } from "react-intersection-observer";
 
 const Skill = () => {
   useEffect(() => {
@@ -13,19 +12,42 @@ const Skill = () => {
   }, []);
 
   const myRef = useRef();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.2, // Adjust this threshold as needed
+  });
+
   useEffect(() => {
-    console.log("myref", myRef.current);
-  }, []);
+    if (inView) {
+      fillSkillBars();
+    }
+  }, [inView]);
+
+  const fillSkillBars = () => {
+    const skillBars = myRef.current.querySelectorAll(".container-skill");
+
+    skillBars.forEach((bar, index) => {
+      const fill = bar.querySelector(".skill-per");
+      const fillWidth = bar.getAttribute("data-fill");
+      fill.style.width = fillWidth;
+    });
+  };
+
   return (
     <>
-      <section className="Skills" id="skill" style={{ backgroundImage: "url(" + Group + ")" }} data-aos="fade-down">
+      <section
+        className="Skills"
+        id="skill"
+        style={{ backgroundImage: `url(${Group})` }}
+        data-aos="fade-down"
+        ref={ref}
+      >
         <div className="section-wrap">
-          <h2 className="title"> Skills</h2>
+          <h2 className="title">Skills</h2>
           <div className="flex-container">
             <div className="text-container">
-              <h3>Profesional Skills</h3>
-
-              <div className="container-skill" ref={myRef}>
+              <h3>Professional Skills</h3>
+              <div className="container-skill" ref={myRef} data-fill="80%">
                 <div className="skill-name">
                   <p>Html/Sass/Ux</p>
                 </div>
@@ -33,37 +55,33 @@ const Skill = () => {
                   <p>80%</p>
                 </div>
               </div>
-
-              <div className="container-skill">
+              <div className="container-skill" ref={myRef} data-fill="60%">
                 <div className="skill-name">
-                  <p>Javascript/Jquery </p>
+                  <p>Javascript/Jquery</p>
                 </div>
                 <div className="skill-per">
                   <p>60%</p>
                 </div>
               </div>
-
-              <div className="container-skill">
+              <div className="container-skill" ref={myRef} data-fill="30%">
                 <div className="skill-name">
-                  <p>React js/Shopify </p>
+                  <p>React js/Shopify</p>
                 </div>
                 <div className="skill-per">
                   <p>30%</p>
                 </div>
               </div>
-
-              <div className="container-skill">
+              <div className="container-skill" ref={myRef} data-fill="40%">
                 <div className="skill-name">
-                  <p>Asp.net/MySql </p>
+                  <p>Asp.net/MySql</p>
                 </div>
                 <div className="skill-per">
                   <p>40%</p>
                 </div>
               </div>
-
-              <div className="container-skill">
+              <div className="container-skill" ref={myRef} data-fill="30%">
                 <div className="skill-name">
-                  <p>Wordpress/Php </p>
+                  <p>Wordpress/Php</p>
                 </div>
                 <div className="skill-per">
                   <p>30%</p>
