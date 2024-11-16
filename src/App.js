a81603914db7b54e75a09bdcfd899d2e9962a942
mohmmadyaminsurve <yaminsurve909@gmail.com>
2024-11-16 17:46:37 +0530

    under construction changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import "./App.scss";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
-import About from "./components/About";
-import Skill from "./components/Skill";
-import Portfolio from "./components/Portfolio";
-import Process from "./components/Process";
-import Work from "./components/Work";
-import ScrollTop from "./components/ScrollTop";
+// import "./App.scss";
 
 function App() {
-  return (
-    <>
-      {/* <h1> Testing hello there </h1> */}
-      <Navbar />
-      <HeroSection />
-      <About />
-      <Process />
-      <Work />
-      <Skill />
-      <Portfolio />
-      <Footer />
-      <ScrollTop />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
